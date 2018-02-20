37502cb8d82dd42963aace48d36a91443c123e47
Jonathan Braga <jonathanb2br@gmail.com>
2018-02-20 18:35:18 -0300

    added support to burger icon click

--- DesignLibrary/MainActivity.cs
+++ DesignLibrary/MainActivity.cs
@@ -1,18 +1,139 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Android.Support.V4.Widget;
+using SupportFragmentManager = Android.Support.V4.App.FragmentManager;
+using SupportFragment = Android.Support.V4.App.Fragment;
+using SupportToolbar = Android.Support.V7.Widget.Toolbar;
+using SupportActionbar = Android.Support.V7.App.ActionBar;
+using Android.Support.V7.App;
+using Android.Support.Design.Widget;
+using System;
+using Android.Support.V4.View;
+using Android.Support.V4.App;
+using System.Collections.Generic;
+using Java.Lang;
+using Android.Views;
+using Android.Content;
 
 namespace DesignLibrary
 {
     [Activity(Label = "DesignLibrary", MainLauncher = true, Theme = "@style/Theme.DesignDemo")]
-    public class MainActivity : Activity
+    public class MainActivity : AppCompatActivity
     {
+        private DrawerLayout mDrawerLayout;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
+
+            SupportToolbar toolBar = FindViewById<SupportToolbar>(Resource.Id.toolBar);
+            SetSupportActionBar(toolBar);
+
+            // Create icon burguer
+            SupportActionbar ab = SupportActionBar;
+            ab.SetHomeAsUpIndicator(Resource.Drawable.ic_menu);
+            ab.SetDisplayHomeAsUpEnabled(true);
+
+            mDrawerLayout = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
+
+            NavigationView navigationView = FindViewById<NavigationView>(Resource.Id.nav_view);
+            if (navigationView != null)
+                SetUpDrawerContent(navigationView);
+
+            TabLayout tabs = FindViewById<TabLayout>(Resource.Id.tabs);
+            ViewPager viewPager = FindViewById<ViewPager>(Resource.Id.viewpager);
+
+            SetUpViewPager(viewPager);
+
+            tabs.SetupWithViewPager(viewPager);
+
+            FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
+            fab.Click += Fab_Click;
+
+        }
+
+        private void Fab_Click(object sender, EventArgs e)
+        {
+            View achor = sender as View;
+            Snackbar.Make(achor, "Yay SnackBar!!!", Snackbar.LengthLong)
+                .SetAction("Action", v =>
+                {
+                    //Intent intent = new Intent();
+                }).Show();
+        }
+
+        private void SetUpViewPager(ViewPager viewPager)
+        {
+            TabAdpter adpter = new TabAdpter(SupportFragmentManager);
+            adpter.AddFragment(null,"");
+            adpter.AddFragment(null,"");
+            adpter.AddFragment(null,"");
+
+            viewPager.Adapter = adpter;
+        }
+
+        private void SetUpDrawerContent(NavigationView navigationView)
+        {
+            navigationView.NavigationItemSelected += NavigationView_NavigationItemSelected;
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            switch (item.ItemId)
+            {
+                case Android.Resource.Id.Home:
+                    mDrawerLayout.OpenDrawer((int)GravityFlags.Left);
+                    return true;
+                default:
+                    return base.OnOptionsItemSelected(item);
+            }
+
+            return base.OnOptionsItemSelected(item);
+        }
+
+        private void NavigationView_NavigationItemSelected(object sender, NavigationView.NavigationItemSelectedEventArgs e)
+        {
+            e.MenuItem.SetChecked(true);
+            mDrawerLayout.CloseDrawers();
+        }
+
+        public class TabAdpter : FragmentPagerAdapter
+        {
+            public List<SupportFragment> Fragments { get; set; }
+            public List<string> FragmentNames { get; set; }
+
+            public TabAdpter(SupportFragmentManager sfm) : base (sfm)
+            {
+                Fragments = new List<SupportFragment>();
+                FragmentNames = new List<string>();
+            }
+
+            public void AddFragment(SupportFragment fragment, string name)
+            {
+                Fragments.Add(fragment);
+                FragmentNames.Add(name);
+            }
+
+            public override int Count
+            {
+                get
+                {
+                    return Fragments.Count;
+                }
+            }
+
+            public override SupportFragment GetItem(int position)
+            {
+                return Fragments[position];
+            }
+
+            public override ICharSequence GetPageTitleFormatted(int position)
+            {
+                return new Java.Lang.String(FragmentNames[position]);
+            }
         }
     }
 }
